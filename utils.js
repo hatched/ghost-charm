1ac51c7b366861eed6b9753ef4559c141b4e902c
j.c.sackett <j.c.sackett@gmail.com>
2013-10-22 21:56:02 -0700

    Moved functions useful for other hooks into utils.js

---
+++ utils.js
@@ -0,0 +1,31 @@
+/**
+  Writes the supplied string to the configuration file.
+
+  @param {String} fileData The completed configuration file string.
+*/
+function writeConfigToFile(fileData) {
+  fs.writeFileSync('/home/ubuntu/ghost/config.js', fileData, 'utf8');
+}
+
+
+/**
+  Substitutes the values in the template with the charm
+  configuration values
+
+  @method substituteValues
+  @param {Object} err exec command error object.
+  @param {Object} tmplFile Config file template contents.
+*/
+function substituteValues(err, config) {
+  config = JSON.parse(config);
+
+  Object.keys(config).forEach(function(key) {
+    fileData = fileData.replace('{{' + key + '}}', config[key]);
+  });
+
+  writeConfigToFile(fileData);
+}
+
+module.exports = {
+  substituteValues: substituteValues
+};
