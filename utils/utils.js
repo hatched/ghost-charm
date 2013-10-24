d9ce4fd67c13e640509cea231384ff6b074823c2
j.c.sackett <j.c.sackett@gmail.com>
2013-10-24 12:17:48 -0700

    Abstract template rendering code separate from updating config.

--- utils/utils.js
+++ utils/utils.js
@@ -1,35 +1,24 @@
 var path = require('path'),
     fs = require('fs');
-/**
-  Writes the supplied string to the configuration file.
-
-  @param {String} fileData The completed configuration file string.
-*/
-function writeConfigToFile(fileData) {
-  fs.writeFileSync('/home/ubuntu/ghost/config.js', fileData, 'utf8');
-}
-
 
 /**
-  Substitutes the values in the template with the charm
-  configuration values
+  Render a template file with the config data.
 
-  @method substituteValues
-  @param {Object} err exec command error object.
-  @param {Object} tmplFile Config file template contents.
+  @method renderTemplateData
+  @param {Object} data Config data
+  @param {String} template The template filename
+  @param {String} filepath Path to render the template out to 
 */
-function substituteValues(err, config) {
-  var fileData = fs.readFileSync(
-    path.join(__dirname, 'assets/config.js.template'), 'utf8');
-  config = JSON.parse(config);
+function renderTemplate(data, template, filepath) {
+  var fileData = fs.readFileSync(path.join('assets', 'template'), 'utf8');
 
   Object.keys(config).forEach(function(key) {
     fileData = fileData.replace('{{' + key + '}}', config[key]);
   });
 
-  writeConfigToFile(fileData);
+  fs.writeFileSync(filepath, fileData, 'utf8');
 }
 
 module.exports = {
-  substituteValues: substituteValues
+  renderTemplate: renderTemplate
 };
