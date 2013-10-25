4f482b2d2eb64716143c738b8feb9baa0aaa6208
Jeff Pihach <hatched@fromanegg.com>
2013-10-25 09:02:56 -0700

    fixed utils placement from merge

---
+++ utils/utils.js
@@ -0,0 +1,24 @@
+var path = require('path'),
+    fs = require('fs');
+
+/**
+  Render a template file with the config data.
+
+  @method renderTemplateData
+  @param {Object} data Config data
+  @param {String} template The template filename
+    @param {String} filepath Path to render the template out to 
+*/
+function renderTemplate(data, template, filepath) {
+  var fileData = fs.readFileSync(path.join('assets', template), 'utf8');
+
+  Object.keys(data).forEach(function(key) {
+    fileData = fileData.replace('{{' + key + '}}', data[key]);
+  });
+
+  fs.writeFileSync(filepath, fileData, 'utf8');
+}
+
+module.exports = {
+  renderTemplate: renderTemplate
+};
